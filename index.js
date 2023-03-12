66d250ef0d77513be56acff4c135fb9b20651043
Syha Ban <syhaban@hotmail.com>
2023-03-11 21:18:14 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const name = ["Guadalupe,", "Ollie,", "Aki"];
+
+function writeCards(names, eventNames) {
+    let messages = []
+       for (let i = 0; i < names.length; i++) { 
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`);
+    } 
+        return messages;
+    }
+
+function countDown(number){
+   while (number >= 0){
+    console.log(number);       
+    number --;
+}
+}
